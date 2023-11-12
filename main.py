322680f144714b643b7ba122655e39e888e1a962
AndersGrunge <morettiandrewd@gmail.com>
2023-11-12 12:36:20 +0300

    JWT delta time 7 days

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ MAX_CONTENT_LENGTH = 1024 * 1024 * 3
 
 app = Flask(__name__)
 app.config["JWT_SECRET_KEY"] = "sample_key"
-app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(days=8)
 app.config.from_object(__name__)
 app.config.from_object(Config)
 app.config.update(dict(DATABASE=os.path.join(app.root_path, 'flsite.db')))
@@ -58,7 +58,7 @@ def refresh_expiring_jwts(response):
     try:
         exp_timestamp = get_jwt()["exp"]
         now = datetime.now(timezone.utc)
-        target_timestamp = datetime.timestamp(now + timedelta(minutes=30))
+        target_timestamp = datetime.timestamp(now + timedelta(days=1))
         if target_timestamp > exp_timestamp:
             access_token = create_access_token(identity=get_jwt_identity())
             data = response.get_json()
@@ -70,15 +70,6 @@ def refresh_expiring_jwts(response):
         return response
 
 
-@app.route('/')
-def index():
-    response_body = {
-        "hello": "World",
-        "test": "success"
-    }
-
-    return response_body
-
 
 @app.route('/login', methods=["POST"])
 def login():
@@ -89,7 +80,7 @@ def login():
         response = {"access_token": access_token}
         return response
     else:
-        response = {"status": "Неверный логин или пароль"}
+        response = {"status": "неверный логин или пароль"}
         return response
 
 @app.route("/register", methods=["POST"])
@@ -102,7 +93,7 @@ def register():
         if len(user_login) > 4 and len(user_mail) > 4 and len(user_psw) > 4 and user_psw == user_psw2:
             psw_hash = generate_password_hash(user_psw)
             if dbase.add_new_user(user_login, user_mail, psw_hash):
-                return {"status": "вы зарегались"}
+                return {"status": "вы зарегистрировались"}
             else:
                 return {"status": "ошибка БД или пользователь уже есть"}
         else:
@@ -115,15 +106,13 @@ def logout():
     return response
 
 
+
 @app.route('/profile', methods=["POST"])
 @jwt_required()
 def my_profile():
-    jwtr = request.headers.get('Authorization', None)
-    jwtr = str.replace(str(jwtr), 'Bearer ', '')
-    token = decode_token(jwtr)
-    email = token["sub"]
+    email = get_jwt_identity()
     user = dbase.get_user_by_email(email)
-    print(user)
+
     response_body = {
         "email": email,
         "login": user["name"]
